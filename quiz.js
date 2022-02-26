4d222dff79b8a8c708d568382e9757cead2ddad7
123jrf <j3r2f1@gmail.com>
2022-02-26 14:16:21 -0600

    Basic website skeleton

---
+++ quiz.js
@@ -0,0 +1,9 @@
+var quizContainer = document.getElementById('quiz-question');
+var resultsContainer = document.getElementById('quiz-results');
+var submitButton = document.getElementById('quiz-submit');
+
+quizContainer.innerHTML = "<p>Hello, world!</p>";
+
+submitButton.onclick() = function {
+	resultsContainer.innerHTML = "<p>Hello, World!</p>";
+}
