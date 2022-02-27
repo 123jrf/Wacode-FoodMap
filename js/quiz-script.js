2d8787f68ee54651ae5e6b86a99311b28e0bc019
123jrf <j3r2f1@gmail.com>
2022-02-27 13:54:36 -0600

    Fixed text styles for quiz

--- js/quiz-script.js
+++ js/quiz-script.js
@@ -1,12 +1,12 @@
 var quizContainer = document.getElementById('quiz-question');
 var submitButton = document.getElementById('quiz-submit');
 var textEntry = document.getElementById("answer_field");
-var answerLabel = document.getElementById("answer_label");
+var answerLabel = document.getElementById("answer-label");
 
 var questions = [
-	["<p>What percentage of Americans do you think are food insecure before COVID-19?</p>", 12.5,
+	["<h3>What percentage of Americans do you think are food insecure before COVID-19?</h3>", 12.5,
 		"<p>In 2020, an estimated 1 in 8 <b>(12.5%)</b> Americans were food insecure, equating to over <strong>38 million</strong> Americans, including almost <strong>12 million children</strong>. <a href=https://hungerandhealth.feedingamerica.org/understand-food-insecurity/>[Source]</a></p>"],
-	["<p>What percentage of Americans do you think are food insecure since the pandemic?</p>", 25,
+	["<h3>What percentage of Americans do you think are food insecure since the pandemic?</h3>", 25,
 		"<p>Since the onset of COVID-19, food insecurity has <strong>doubled</strong> to around <strong>25%</strong> in the United States.</p>"
 	]
 ]
@@ -40,14 +40,15 @@ function quizbtnpress() {
 			console.log("No");
 		}
 		else if ((inputVal > answerVal*0.9) && (inputVal < answerVal*1.1)) {
-			result = "<p>Correct!</p>";
+			result = "<p><b>Correct!</b></p>";
 		}
 		else {
-			result = "<p>Incorrect!</p>";
+			result = "<p><b>Incorrect!</b></p>";
 		}
 		
 		// Set the results text
 		textEntry.style.visibility = "hidden";
+		answerLabel.innerHTML = "Your answer: <b>" + inputVal.toString() + "%</b>";
 		quizContainer.innerHTML = result + questions[(current_question) / 2][2];
 		
 	}
@@ -55,6 +56,7 @@ function quizbtnpress() {
 		quizContainer.innerHTML = questions[(current_question+1) / 2][0];
 		
 		textEntry.style.visibility = "visible";
+		answerLabel.innerHTML = "Enter your answer:";
 		
 	}
 	
